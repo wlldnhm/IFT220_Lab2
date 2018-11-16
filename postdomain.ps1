45ec0ae0bbce6e227a2986aa7c98f1c108b69671
Eric Case <44282950+ericcase@users.noreply.github.com>
2018-11-15 19:44:01 -0700

    Update postdomain.ps1

--- postdomain.ps1
+++ postdomain.ps1
@@ -1,4 +1,4 @@
-# Run this after the machine reboot and check the output for errors
+# Run this after the machine reboots and check the output for errors
 
 dcdiag
 Get-Service adws,kdc,netlogon,dns
