50a786a0aa9d5bf3b9ccbbfee2bb7b01fd5bad66
Eric Case <44282950+ericcase@users.noreply.github.com>
2018-11-23 14:28:42 -0700

    quick edit

--- domain.ps1
+++ domain.ps1
@@ -1,6 +1,6 @@
 # Builds a new DC in a new Forest
 # Assumption: new machine uses DHCP
-
+# quick edit
 # Change from DHCP to static IP using the same IP
 # Get the name of the network adapter
 $nicname = Get-NetAdapter  | select -ExpandProperty "name"
