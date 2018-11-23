a61cf99f534f87cbe2301e588be7c52a6de9a512
Eric Case <ericcase@asu.edu>
2018-11-22 17:40:54 -0700

    Network loging

--- domain.ps1
+++ domain.ps1
@@ -4,11 +4,14 @@
 # Change from DHCP to static IP using the same IP
 # Get the name of the network adapter
 $nicname = Get-NetAdapter  | select -ExpandProperty "name"
+Write-Host -ForegroundColor yellow $nicname 
 
 # Get current IP Address, Prefix Length (subnet mask), and gateway
 $ipaddress = Get-NetIPAddress -InterfaceAlias $nicname -AddressFamily IPv4 | select -ExpandProperty "IPAddress"
 $prefixlength = Get-NetIPAddress -InterfaceAlias $nicname -AddressFamily IPv4 | select -ExpandProperty "PrefixLength"
 $gateway = Get-NetIPConfiguration -InterfaceAlias $nicname | select -ExpandProperty "IPv4DefaultGateway" | select -ExpandProperty "NextHop"
+Write-Host -ForegroundColor yellow $ipaddress/$prefixlength 
+Write-Host -ForegroundColor yellow $gateway 
 
 # Set the current IP address as static
 Remove-NetIPAddress -InterfaceAlias $nicname -AddressFamily IPv4 -Confirm:$false
